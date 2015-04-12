319e67c56bda170894f403a7ec31d2f502913166
V1c70r <V1c70r@example.com>
2015-04-12 18:33:15 +0300

    add permutation function

--- spec/solveSpec.js
+++ spec/solveSpec.js
@@ -1,57 +1,137 @@
 'use strict';
 
 var factorial = require('../solve').factorial;
+var uniq = require('../solve').uniq;
+var without = require('../solve').without;
+var permutation = require('../solve').permutation;
 
-describe('factorial', function () {
-  it('should return 1 for 0', function () {
+describe('factorial', () => {
+  it('should return 1 for 0', () => {
     expect(factorial(0)).toBe(1);
   });
 
-  it('should return 1 for 1', function () {
+  it('should return 1 for 1', () => {
     expect(factorial(1)).toBe(1);
   });
 
-  it('should return 2 for 2', function () {
+  it('should return 2 for 2', () => {
     expect(factorial(2)).toBe(2);
   });
 
-  it('should return 6 for 3', function () {
+  it('should return 6 for 3', () => {
     expect(factorial(3)).toBe(6);
   });
 
-  it('should return 24 for 4', function () {
+  it('should return 24 for 4', () => {
     expect(factorial(4)).toBe(24);
   });
 
-  it('should return 120 for 5', function () {
+  it('should return 120 for 5', () => {
     expect(factorial(5)).toBe(120);
   });
 
-  it('should return 720 for 6', function () {
+  it('should return 720 for 6', () => {
     expect(factorial(6)).toBe(720);
   });
 
-  it('should return 5040 for 7', function () {
+  it('should return 5040 for 7', () => {
     expect(factorial(7)).toBe(5040);
   });
 
-  it('should return 40 320 for 8', function () {
+  it('should return 40 320 for 8', () => {
     expect(factorial(8)).toBe(40320);
   });
 
-  it('should return 362 880 for 9', function () {
+  it('should return 362 880 for 9', () => {
     expect(factorial(9)).toBe(362880);
   });
 
-  it('should return 3 628 800 for 10', function () {
+  it('should return 3 628 800 for 10', () => {
     expect(factorial(10)).toBe(3628800);
   });
 });
 
-//describe('permutation', function () {
-//  it('should return a generator for an empty array');
-//  it('should return a generator for an array with one item');
-//  it('should return all permutation for an array with two item');
-//  it('should return all permutation for an array with three item');
-//  it('should return all permutation for an array with five item');
-//});
+describe('uniq', () => {
+  it('should return an empty array', () => {
+    expect(uniq([])).toEqual([]);
+  });
+
+  it('should return an array with one item', () => {
+    expect(uniq(['a'])).toEqual(['a']);
+  });
+
+  it('should return unique elements of array', () => {
+    expect(uniq([1, 2, 3, 'a', 'b', 'c'])).toEqual(['1', '2', '3', 'a', 'b', 'c']);
+    expect(uniq([1, 2, 3, 'a', 'b', 'c', 'c', 'b', 'b', 'a', 3, 2, 1])).toEqual(['1', '2', '3', 'a', 'b', 'c']);
+  });
+})
+
+describe('without', () => {
+  it('should not change an original array', () => {
+    var ar = ['a', 'b', 'c', 'd', 'e'];
+    without(ar, 1)
+    expect(ar).toEqual(['a', 'b', 'c', 'd', 'e']);
+  });
+
+  it('should return new array without one element', () => {
+    expect(without(['a', 'b', 'c', 'd', 'e'], 1)).toEqual(['a', 'c', 'd', 'e']);
+  });
+
+  it('should return original array for indexes bigger than an array length', () => {
+    expect(without([], 10)).toEqual([]);
+    expect(without(['a', 'b', 'c', 'd', 'e'], 10)).toEqual(['a', 'b', 'c', 'd', 'e']);
+  });
+});
+
+describe('permutation', () => {
+  function allValues(generator) {
+    var values = [];
+
+    for (var value of generator) {
+      values.push(value);
+    }
+
+    return values;
+  }
+
+  it('should return a generator for an empty array', () => {
+    expect(allValues(permutation([]))).toEqual([[]]);
+  });
+
+  it('should return a generator for an array with one item', () => {
+    expect(allValues(permutation(['a']))).toEqual([['a']]);
+  });
+
+  it('should return all permutation for an array with two item', () => {
+    expect(allValues(permutation(['a', 'b']))).toEqual([
+      ['a', 'b'],
+      ['b', 'a']
+    ]);
+  });
+
+  it('should return all permutation for an array with three item', () => {
+    expect(allValues(permutation(['a', 'b', 'c'])).sort()).toEqual([
+      ['a', 'b', 'c'],
+      ['a', 'c', 'b'],
+      ['b', 'a', 'c'],
+      ['b', 'c', 'a'],
+      ['c', 'a', 'b'],
+      ['c', 'b', 'a']
+    ]);
+  });
+
+  it('should return all permutation for an array with five item', () => {
+    var values = allValues(permutation(['a', 'b', 'c', 'd', 'e']));
+
+    expect(values.length).toBe(5 * 4 * 3 * 2 * 1);
+    values.forEach(function (array) {
+      expect(array.indexOf('a')).not.toBe(-1);
+      expect(array.indexOf('b')).not.toBe(-1);
+      expect(array.indexOf('c')).not.toBe(-1);
+      expect(array.indexOf('d')).not.toBe(-1);
+      expect(array.indexOf('e')).not.toBe(-1);
+    });
+
+    expect(uniq(values.map(v => String(v))).length).toBe(5 * 4 * 3 * 2 * 1);
+  });
+});
